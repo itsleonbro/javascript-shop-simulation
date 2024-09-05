e3babb68067ef9efac5bfd78f0f8562f4261ce07
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2024-09-05 16:48:18 +0200

    display all products and their prices

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const products = ["pen", "notebook", "backpack", "water bottle", "calculator"];
 const prices = [10, 50, 300, 150, 500];
 let userBudget = 500;
 let userCart = {};
+
+// function to display all products and their prices
+const displayProducts = () => {
+  console.log("Available products:");
+  for (let i = 0; i < products.length; i++) {
+    console.log(`${i + 1}. ${products[i]} - R${prices[i]}`);
+  }
+};
